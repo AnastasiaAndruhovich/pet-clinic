3b9e02bae8da3f91dac71d83084c96c589de7cdc
Anastasia-Andruhovich <anastasia.andruhovich@ibm.com>
2021-09-27 22:39:43 +0300

    Add Pets and Contact Info on startup. Closes #35

--- pet-clinic-data/src/main/java/by/andruhovich/petclinic/service/map/OwnerServiceMap.java
+++ pet-clinic-data/src/main/java/by/andruhovich/petclinic/service/map/OwnerServiceMap.java
@@ -1,13 +1,24 @@
 package by.andruhovich.petclinic.service.map;
 
 import by.andruhovich.petclinic.model.Owner;
+import by.andruhovich.petclinic.model.Pet;
 import by.andruhovich.petclinic.service.OwnerService;
+import by.andruhovich.petclinic.service.PetService;
+import by.andruhovich.petclinic.service.PetTypeService;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
 @Service
 public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements OwnerService {
+    private final PetTypeService petTypeService;
+    private final PetService petService;
+
+    public OwnerServiceMap(PetTypeService petTypeService, PetService petService) {
+        this.petTypeService = petTypeService;
+        this.petService = petService;
+    }
+
     @Override
     public Set<Owner> findAll() {
         return super.findAll();
@@ -20,7 +31,24 @@ public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements 
 
     @Override
     public Owner save(Owner object) {
-        return super.save(object);
+        if (object != null) {
+            if (object.getPets() != null) {
+                object.getPets().forEach(pet -> {
+                    if (pet.getPetType() != null) {
+                        pet.setPetType(petTypeService.save(pet.getPetType()));
+                    } else {
+                        throw new RuntimeException("Pet Type is required");
+                    }
+
+                    if (pet.getId() != null) {
+                        Pet savedPet = petService.save(pet);
+                        pet.setId(savedPet.getId());
+                    }
+                });
+            }
+            return super.save(object);
+        }
+        return null;
     }
 
     @Override
